2c0bb83dd1f2b59d32a784c80edd1c703116b2f3
Oana <talos.oanamaria@gmail.com>
2025-07-17 11:46:48 +0300

    added configuration for mcp server, set headless mode to false

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -12,9 +12,12 @@ test('get started link', async ({ page }) => {
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.context().browser()?.close();
+
 });
 
 test("goto vs launch", async({ page }) => {
